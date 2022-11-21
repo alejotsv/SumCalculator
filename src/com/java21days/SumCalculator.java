deeb22b95dcafdf5cc915d942520fd923ce8a6a7
Alejandro Salas <asalas.wd@outlook.com>
2022-11-21 11:26:13 -0500

    Initial commit

---
+++ src/com/java21days/SumCalculator.java
@@ -0,0 +1,19 @@
+package com.java21days;
+
+import javax.swing.*;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+
+public class SumCalculator extends JFrame implements FocusListener {
+
+
+    @Override
+    public void focusGained(FocusEvent focusEvent) {
+
+    }
+
+    @Override
+    public void focusLost(FocusEvent focusEvent) {
+
+    }
+}
