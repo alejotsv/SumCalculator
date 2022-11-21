081e909492df6da74f0cc68f1bf79f077d3cfb71
Alejandro Salas <asalas.wd@outlook.com>
2022-11-21 11:28:15 -0500

    Generate basic constructor

--- src/com/java21days/SumCalculator.java
+++ src/com/java21days/SumCalculator.java
@@ -1,11 +1,20 @@
 package com.java21days;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
 
 public class SumCalculator extends JFrame implements FocusListener {
+    Dimension dim = new Dimension(300, 300);
 
+    SumCalculator(String title){
+        super(title);
+        setSize(dim);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
 
     @Override
     public void focusGained(FocusEvent focusEvent) {
